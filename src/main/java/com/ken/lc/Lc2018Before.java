d9250a8443be574e2facaf89af780d19fe043378
solo <1064190295@qq.com>
2019-03-18 23:08:38 +0800

    update search matrix

--- src/main/java/com/ken/lc/Lc2018Before.java
+++ src/main/java/com/ken/lc/Lc2018Before.java
@@ -62,6 +62,10 @@ public class Lc2018Before {
 	/**
 	 * 搜索二维矩阵 II<br/>
 	 * https://leetcode-cn.com/explore/featured/card/top-interview-quesitons-in-2018/261/before-you-start/1108/
+	 * <p>
+	 * 减治法：
+	 * 右上角数值为一行中最大值，为一列中最小值。通过右上角和目标值比较，右上角值大，则列数减一；右上角值小，则行数加一。
+	 * </p>
 	 *
 	 * @param matrix
 	 * @param target
@@ -82,6 +86,51 @@ public class Lc2018Before {
 			} else {
 				m++;
 			}
+		}
+		return result;
+	}
+
+	/**
+	 * 搜索二维矩阵 II<br/>
+	 * https://leetcode-cn.com/explore/featured/card/top-interview-quesitons-in-2018/261/before-you-start/1108/
+	 * <p>
+	 * 二分法：
+	 * 根据二分法不断缩小矩阵，缩小至2*2（2*1 or 1*2）矩阵后直接比较
+	 * </p>
+	 *
+	 * @param matrix
+	 * @param target
+	 * @return
+	 */
+	public static boolean searchMatrixEx(int[][] matrix, int target) {
+
+		if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return false;
+		}
+
+		int m = matrix.length - 1;
+		int n = matrix[0].length - 1;
+		int m0 = 0, n0 = 0;
+		int m1 = m, n1 = n;
+		boolean result = false;
+		int mt,nt;
+
+		while (m1 - m0 > 1 || n1 - n0 > 1) {
+			mt = m1 - m0 > 1 ? (m0 + m1) / 2 : m0;
+			nt = n1 - n0 > 1 ? (n0 + n1) / 2 : n0;
+			if (matrix[mt][nt] == target) {
+				result = true;
+				break;
+			} else if (matrix[mt][nt] > target) {
+				m1 = mt;
+				n1 = nt;
+			} else {
+				m0 = mt;
+				n0 = nt;
+			}
+		}
+		if (matrix[m0][n0] == target || matrix[m0][n1] == target || matrix[m1][n0] == target || matrix[m1][n1] == target) {
+			result = true;
 		}
 		return result;
 	}
--- src/test/java/com/ken/lc/Lc2018BeforeTest.java
+++ src/test/java/com/ken/lc/Lc2018BeforeTest.java
@@ -2,20 +2,17 @@ package com.ken.lc;
 
 import org.junit.Assert;
 import org.junit.Test;
-import sun.awt.SunHints;
-
-import java.util.stream.IntStream;
 
 public class Lc2018BeforeTest {
 
 	@Test
 	public void testSingleNum() {
 
-		int[] pa = {2,2,1};
+		int[] pa = {2, 2, 1};
 		int ra = Lc2018Before.singleNumber(pa);
 		Assert.assertEquals(1, ra);
 
-		int[] pb = {4,1,2,1,2};
+		int[] pb = {4, 1, 2, 1, 2};
 		int rb = Lc2018Before.singleNumber(pb);
 		Assert.assertEquals(4, rb);
 	}
@@ -23,10 +20,10 @@ public class Lc2018BeforeTest {
 	@Test
 	public void testMajorityElement() {
 
-		int[] pa = {3,2,3};
+		int[] pa = {3, 2, 3};
 		int ra = Lc2018Before.majorityElement(pa);
 		Assert.assertEquals(3, ra);
-		int[] pb = {2,2,1,1,1,2,2};
+		int[] pb = {2, 2, 1, 1, 1, 2, 2};
 		int rb = Lc2018Before.majorityElement(pb);
 		Assert.assertEquals(2, rb);
 	}
@@ -34,19 +31,50 @@ public class Lc2018BeforeTest {
 	@Test
 	public void testSearchMatrix() {
 
-		int[][] p = {{1,   4,  7, 11, 15},
-				{2,   5,  8, 12, 19},
-				{3,   6,  9, 16, 22},
+		int[][] p = {{1, 4, 7, 11, 15},
+				{2, 5, 8, 12, 19},
+				{3, 6, 9, 16, 22},
 				{10, 13, 14, 17, 24},
 				{18, 21, 23, 26, 30}};
 		int ta = 5;
 		int tb = 20;
 		boolean ra = Lc2018Before.searchMatrix(p, ta);
-		boolean rb = Lc2018Before.searchMatrix(p, tb );
+		boolean rb = Lc2018Before.searchMatrix(p, tb);
 		Assert.assertTrue(ra);
 		Assert.assertFalse(rb);
+	}
 
-		IntStream.of(p[0]).forEach(System.out::println);
+	@Test
+	public void testSearchMatrixEx() {
 
+		int[][] p = {{1, 4, 7, 11, 15},
+				{2, 5, 8, 12, 19},
+				{3, 6, 9, 16, 22},
+				{10, 13, 14, 17, 24},
+				{18, 21, 23, 26, 30}};
+		int ta = 5;
+		int tb = 20;
+		boolean ra = Lc2018Before.searchMatrixEx(p, ta);
+		boolean rb = Lc2018Before.searchMatrixEx(p, tb);
+		Assert.assertTrue(ra);
+		Assert.assertFalse(rb);
+	}
+
+	@Test
+	public void testSearchMatrixEx1() {
+
+		int[][] p = {{-5}};
+		int ta = 1;
+		boolean ra = Lc2018Before.searchMatrixEx(p, ta);
+		Assert.assertFalse(ra);
+	}
+
+	@Test
+	public void testSearchMatrixEx2() {
+
+		int[][] p = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+		int ta = 15;
+		boolean ra = Lc2018Before.searchMatrixEx(p, ta);
+		Assert.assertTrue(ra);
 	}
 }
